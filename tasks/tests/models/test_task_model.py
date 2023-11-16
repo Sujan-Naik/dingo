a62af06e733e358e668838e04fdb385dfb95e187
George Taylor <george.3.taylor@kcl.ac.uk>
2023-11-16 16:06:45 +0000

    Add tests for priority in task model

---
+++ tasks/tests/models/test_task_model.py
@@ -0,0 +1,49 @@
+"""Unit tests for the Task model."""
+from datetime import timedelta
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils import timezone
+from tasks.models import Task
+
+class TaskModelTestCase(TestCase):
+    """Unit tests for the Task model."""
+
+    fixtures = [
+        'tasks/tests/fixtures/default_user.json',
+        'tasks/tests/fixtures/default_task.json'
+    ]
+    def setUp(self):
+        self.task = Task.objects.get(name='Testing')
+
+        # Make the deadline 1 day from now, so that the task is valid.
+        self.task.deadline = timezone.now() + timedelta(days=1)
+
+    def test_valid_task(self):
+        self._assert_task_is_valid
+
+    def test_priority_cannot_be_blank(self):
+        self.priority = None
+        self._assert_task_is_invalid 
+
+    def test_priority_cannot_be_less_than_1(self):
+        self.priority = 0
+        self._assert_task_is_invalid 
+
+    def test_priority_cannot_be_greater_than_5(self):
+        self.priority = 6
+        self._assert_task_is_invalid 
+
+    def test_priority_can_be_5(self):
+        self.priority = 5
+        self._assert_task_is_valid
+
+
+    def _assert_task_is_valid(self):
+        try:
+            self.task.full_clean()
+        except (ValidationError):
+            self.fail('Test task should be valid')
+
+    def _assert_task_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.task.full_clean()
