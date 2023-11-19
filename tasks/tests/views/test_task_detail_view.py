e7c9876be40fa5792f76f258072d1471e4abc311
Siqi Li <1114673087@qq.com>
2023-11-19 23:39:14 +0000

    add test of task detail and list

---
+++ tasks/tests/views/test_task_detail_view.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.urls import reverse
+from tasks.models import Task
+from datetime import datetime
+
+class TaskDetailViewTest(TestCase):
+
+    def setUpTestData(cls):
+        # set the date for test
+        Task.objects.create(name="Test Task 1",
+                            description = 'test task',
+                            deadline = datetime.now(),
+                            priority = 3)
+
+    def test_task_details_view(self):
+        response = self.client.get(reverse('task_detail', args=[self.task.author]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_details_template(self):
+        response = self.client.get(reverse('task_detail', args=[self.task.author]))
+        self.assertTemplateUsed(response, 'tasks/task_detail.html')
+
+    def test_task_details_view_task(self):
+        response = self.client.get(reverse('task_detail', args=[self.task.author]))
+        self.assertContains(response, 'Test Task 1')
+        self.assertContains(response, 'Due Date')
+        self.assertContains(response, 'Description')
+        self.assertContains(response, 'Author')
+        self.assertContains(response, 'Time Remaining')
+
---
+++ tasks/tests/views/test_task_list_view.py
@@ -0,0 +1,42 @@
+"""Tests of the task list view."""
+
+from django.test import TestCase
+from django.urls import reverse
+
+from tasks.models import Task
+from datetime import datetime
+
+class TaskListTestCase(TestCase):
+    """Tests of the task list view."""
+
+    def setUpTestData(cls):
+        # set the date for test
+        Task.objects.create(name="Test Task 1",
+                            description = 'test task',
+                            deadline = datetime.now(),
+                            priority = 3)
+
+    # test if the task list exists
+    def test_task_list_view(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertEqual(response.status_code,200)
+
+    # test if the template is correct
+    def test_template(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertTemplateUsed(response,'tasks/task_list.html')
+
+    # test if the list can show the correct task
+    def test_task_list_show_task(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertContains(response, 'Test Task 1')
+
+    # test if no task to show
+    def test_no_task(self):
+        pass
+
+    # test if has many tasks to show, the number of tasks
+    def test_task_number(self):
+        pass
+
+
