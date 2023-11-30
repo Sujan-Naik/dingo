4b96929237054b84b1ae96b7c067edcb36c51cb4
Siqi Li <1114673087@qq.com>
2023-11-30 14:34:37 +0000

    add invite_member_test

--- tasks/tests/views/test_team_detail_view.py
+++ tasks/tests/views/test_team_detail_view.py
@@ -24,20 +24,17 @@ class TeamDetailViewTest(TestCase):
         self.team_member1 = User.objects.get(username='@johndoe')
         self.team_member2 = User.objects.get(username='@janedoe')
         self.team.team_members.add(self.team_member1, self.team_member2)
+        self.response = self.client.get(reverse('team_detail', args=[self.team.team_name]))
 
-    def test_task_details_view(self):
+    def test_team_details_view(self):
         self.client.login(username=self.user.username, password='Password123')
-        team = Team.objects.get(team_name='test_team')
-        response = self.client.get(reverse('team_detail',args=[team.team_name]))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
-    def test_task_details_template(self):
+    def test_team_details_template(self):
         self.client.login(username=self.user.username, password='Password123')
-        team = Team.objects.get(team_name='test_team')
-        response = self.client.get(reverse('team_detail',args=[team.team_name]))
-        self.assertTemplateUsed(response, 'team_detail.html')
+        self.assertTemplateUsed(self.response, 'team_detail.html')
 
-    def test_task_details_view_task(self):
+    def test_team_details_view(self):
         self.client.login(username=self.user.username, password='Password123')
         team = Team.objects.get(team_name='test_team')
         response = self.client.get(reverse('team_detail',args=[team.team_name]))
@@ -45,3 +42,12 @@ class TeamDetailViewTest(TestCase):
         self.assertContains(response, team.team_name)
         self.assertContains(response, self.team_member1.username)
         self.assertContains(response, self.team_member2.username)
+
+    def test_invite_member_success(self):
+        self.client.login(username=self.user.username, password='Password123')
+        team = Team.objects.get(team_name='test_team')
+        invited_user = User.objects.create_user(username='@inviteduser', password='Password123')
+        self.form_data['action'] = 'invite'
+        response = self.client.get(reverse('team_detail', args=[team.team_name]))
+
+
