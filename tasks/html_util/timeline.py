0bfaab824d5d008e0a3193e989f0df9d15b47714
George Taylor <george.3.taylor@kcl.ac.uk>
2023-12-13 19:54:10 +0000

    Fix timeline to work with timezones

--- tasks/html_util/timeline.py
+++ tasks/html_util/timeline.py
@@ -2,6 +2,7 @@ import calendar
 from calendar import HTMLCalendar, monthrange
 
 from django.db.models.functions import datetime
+from django.utils.timezone import get_current_timezone
 
 from tasks.models import Task
 
@@ -19,8 +20,9 @@ class Timeline(HTMLCalendar):
         year = self.current_year
         month = self.current_month
         date = datetime.datetime(year, month, day)
-        day_tasks = self.tasks.filter(deadline__range=[datetime.datetime.combine(date, datetime.datetime.min.time()),
-                                                       datetime.datetime.combine(date, datetime.datetime.max.time())])
+        tz = get_current_timezone()
+        day_tasks = self.tasks.filter(deadline__range=[datetime.datetime.combine(date, datetime.datetime.min.time(), tz),
+                                                       datetime.datetime.combine(date, datetime.datetime.max.time(), tz)])
         html = f'<div class="col-3"><span class="border">{day}'
         if len(day_tasks) != 0:
             for task in day_tasks:
@@ -28,7 +30,7 @@ class Timeline(HTMLCalendar):
         html += '</span> </div>'
         return html
 
-    def formatmonth(self, year, month, withyear=True):#
+    def formatmonth(self, year, month, withyear=True):
         """Displays tasks for a given Month"""
         self.current_year = year
         self.current_month = month
@@ -53,7 +55,7 @@ class Timeline(HTMLCalendar):
     def returnHTMLPages(self):
         """Displays tasks for from 2023 to the current year + 5"""
         oldest_date = 2023
-        current_date = datetime.timezone.now().date().year + 5
+        current_date = datetime.timezone.localdate(datetime.timezone.now()).year + 5
         pagination = '<nav aria-label="navigation"> <ul class="pagination justify-content-center">'
         html = ''
         for year in range(oldest_date, current_date):
